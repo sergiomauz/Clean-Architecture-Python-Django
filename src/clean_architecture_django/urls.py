190433074ab4054ba4c28cea5bc11426851ee099
Sergio Mauricio Zambrano <sergio.mauz88@gmail.com>
2022-08-27 18:09:55 -0500

    Avance

--- src/clean_architecture_django/urls.py
+++ src/clean_architecture_django/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -35,12 +35,10 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
-
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
-            name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     path('admin/', admin.site.urls),
 
     path('api/v1/', include("default_app.api.urls")),
