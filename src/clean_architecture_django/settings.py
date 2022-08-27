7147356d02a64df1ee2355ebf82800a218e0a5dc
Sergio Mauricio Zambrano <sergio.mauz88@gmail.com>
2022-08-27 17:51:40 -0500

    Avance

--- src/clean_architecture_django/settings.py
+++ src/clean_architecture_django/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%xhe^rce@5ulovw1zww@0&z2^j1psu*(opa7*vugc%7u*1*wfj'
+SECRET_KEY = os.environ.get('SECRET_KEY', '1234567890')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
